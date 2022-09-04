c90838b9295a44fc6b1a01639695bfe1740f99dc
uzair abid <uzairabid1001@gmial.com>
2022-09-04 20:21:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let a ={
+    iphone:{
+        v14pro:{
+             ram:4,
+             rom:64,
+             price:22,
+        },
+    },
+    
+}
+const b = {...a.iphone.v14pro}
+console.log(b)
+
+
+const c = ["1","2","3","4",
+     mode =
+          ["5","6","7","8",
+        average =
+                ["9","10","11","12"
+    ],
+    ],
+     
+]
+const d = [...average]
+console.log(d)
